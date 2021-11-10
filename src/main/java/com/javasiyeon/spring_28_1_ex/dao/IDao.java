fb728a30bc863ac2c6cf56818a8de0fcd5696d5f
samsung <samsung@DESKTOP-NNK3FDU>
2021-11-10 17:20:40 +0900

    mybatis 2021년 11월 10일 v.1.

---
+++ src/main/java/com/javasiyeon/spring_28_1_ex/dao/IDao.java
@@ -0,0 +1,12 @@
+package com.javasiyeon.spring_28_1_ex.dao;
+
+import java.util.ArrayList;
+
+import com.javasiyeon.spring_28_1_ex.dto.ContentDto;
+
+public interface IDao {
+   
+   public ArrayList<ContentDto> listDao();
+   public void writeDao(String mWriter, String mContent);
+   public void deleteDao(String mId);
+}
